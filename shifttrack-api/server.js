1b6f6bca1f1063ebf3bb73a52ed1702e3cedb78f
Mehdi-Khazaal <khazaalmahdi10@gmail.com>
2026-04-05 16:32:46 -0400

    remove swap system completely

--- shifttrack-api/server.js
+++ shifttrack-api/server.js
@@ -18,7 +18,6 @@ app.use('/api/locations',require('./routes/locations'));
 app.use('/api/schedule', require('./routes/schedule'));
 app.use('/api/settings', require('./routes/settings'));
 app.use('/api/admin',    require('./routes/admin'));
-app.use('/api/swaps', require('./routes/swaps'));
 
 // Health check
 app.get('/health', (req, res) => {
